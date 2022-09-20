916d7bfc51b8e3ec1cf8ba04f440b2c2b1b4c01e
KlrDR <ka.drach04@gmail.com>
2022-09-20 17:30:17 +0300

    Complited third laba

---
+++ laba03/ConsoleApp2/Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programm
+{
+    public partial class Airlains
+    {
+
+        public Airlains(int NofFlight = 0)
+        {
+
+        }
+
+        // private Airlains(){}
+
+        public Airlains(string Destinaton, int NofFlight, string TypeFly, DateTime dateTime, string Day)
+        {
+            this.Destinaton = Destinaton;
+            this.NofFlight = NofFlight;
+            this.TypeFly = TypeFly;
+            this.dateTime = dateTime;
+            this.Day = Day;
+        }
+
+        public Airlains()
+        {
+
+        }
+
+
+        public static void GoalDestination(Airlains[] x)
+        {
+            Console.WriteLine("Рейсы к какому пункту назначения вы хотите узнать?");
+
+            string? destination = Console.ReadLine();
+
+            foreach (Airlains dest in x)
+            {
+                if (destination == dest.Destinaton)
+                {
+                    Console.WriteLine("{0} {1} {2} {3} {4}", dest.Destinaton, dest.NofFlight, dest.TypeFly, dest.dateTime, dest.Day);
+                }
+            }
+        }
+
+
+        public static void SelectionOfDay(Airlains[] Air, out string massage)
+        {
+            massage = "HELLO";
+
+            Console.WriteLine("Рейсы в какой день вы хотите узнать?");
+
+            string? destination = Console.ReadLine();
+
+            foreach (Airlains day in Air)
+            {
+                if (destination == day.Day)
+                {
+                    Console.WriteLine("{0} {1} {2} {3} {4}", day.Destinaton, day.NofFlight, day.TypeFly, day.dateTime, day.Day);
+                }
+            }
+        }
+
+        public override bool Equals(object obj)                 //переопределение метода Equals()
+        {
+            Airlains? temp = obj as Airlains;
+            if (temp == null)
+                return false;
+            return (temp.dateTime == this.dateTime && temp.Day == this.Day && temp.Destinaton == this.Destinaton && temp.NofFlight == this.NofFlight && temp.TypeFly == this.TypeFly);
+        }
+
+        public override int GetHashCode()                       //переопределение метода GetHashCode()
+        {
+            Console.WriteLine("Вызвался переопределенный метод GetHashCode");
+            return 1;
+        }
+
+        public override string ToString()                       //переопределение метода ToString()
+        {
+            return ($"Пункт назначения: {this.Country}, День недели: {this.Day}, Номер рейса: {this.NofFlight}, Тип самолёта: {this.TypeFly}");
+        }
+
+        static int Number;
+
+        static Airlains()
+        {
+
+        }
+
+        public static void PrintList(ref Airlains[] x)
+        {
+            foreach (Airlains xz in x)
+            {
+                Console.WriteLine("{0}  {1}  {2}  {3}  {4}", xz.Destinaton, xz.NofFlight, xz.TypeFly, xz.dateTime, xz.Day);
+            }
+
+        }
+    }
+}
--- laba03/ConsoleApp2/Program.cs
+++ laba03/ConsoleApp2/Program.cs
@@ -3,61 +3,41 @@
 
 namespace Programm
 {
-    public class Airlains
+
+    public partial class Airlains
     {
         private readonly string Destinaton;
         private readonly int NofFlight;
-        private readonly string TypeFly;
-        private readonly DateTime dateTime;
-        private readonly string Day;
+        private string TypeFly;
+        private DateTime dateTime;
+        private string Day;
 
-        public Airlains(string Destinaton, int NofFlight, string TypeFly, DateTime dateTime, string Day)
+        public string Country                       //общедоступные свойства
         {
-            this.Destinaton = Destinaton;
-            this.NofFlight = NofFlight;
-            this.TypeFly = TypeFly;
-            this.dateTime = dateTime;
-            this.Day = Day;
+            get { return this.Country; }
+            private set { this.Country = value; }
         }
 
-       public static void GoalDestination(Airlains[] x)
+        public int Flight
         {
-            Console.WriteLine("Рейсы к какому пункту назначения вы хотите узнать?");
-
-            string? destination = Console.ReadLine();
-
-            foreach ( Airlains dest in x)
-            {
-                if( destination == dest.Destinaton)
-                {
-                    Console.WriteLine("{0} {1} {2} {3} {4}", dest.Destinaton, dest.NofFlight, dest.TypeFly, dest.dateTime, dest.Day);
-                }
-            }
+            get { return this.NofFlight; }        //ограничили доступ по set, т.е. свойство доступно только для чтения
         }
 
-
-        public static void SelectionOfDay(Airlains[] Air)
+        public string Airplane
         {
-            Console.WriteLine("Рейсы в какой день вы хотите узнать?");
-
-            string? destination = Console.ReadLine();
-
-            foreach (Airlains day in Air)
-            {
-                if (destination == day.Day)
-                {
-                    Console.WriteLine("{0} {1} {2} {3} {4}", day.Destinaton, day.NofFlight, day.TypeFly, day.dateTime, day.Day);
-                }
-            }
+            get { return this.TypeFly; }
+            set { this.TypeFly = value; }
         }
 
-       public static void  PrintList(Airlains[] x)
+        public DateTime Flight_Time
         {
-            foreach (Airlains xz in x)
-            {
-                Console.WriteLine("{0}  {1}  {2}  {3}  {4}",xz.Destinaton, xz.NofFlight, xz.TypeFly, xz.dateTime, xz.Day);
-            }
-
+            get { return this.dateTime; }
+            private set { this.dateTime = value; }
+        }
+        public string Day_of_week
+        {
+            get { return this.Day; }
+            set { this.Day = value; }
         }
 
     }
@@ -67,26 +47,28 @@ namespace Programm
         public static void Main()
         {
 
-       
+            string massage = "HI";
 
 
-            Airlains[] reys = new Airlains[13] { new Airlains("Moskow", 1, "boeng3777", new DateTime(2022, 09, 28, 19, 00, 00), "Mondeay"),
+            var flight = new { destination = "Ghonkong", NofFlight = 1, TypeFly = "Boeing777", dateTime = new DateTime(2021, 09, 03), Day = "Воскресенье" };
+
+            Airlains[] reys = new Airlains[13] {new Airlains("Moskow", 1, "boeng3777", new DateTime(2022, 09, 28, 19, 00, 00), "Mondeay"),
                                                 new Airlains("New-York", 2, "boeng31427", new DateTime(2022, 12, 08, 01, 30, 00), "Tusday"),
                                                 new Airlains("Minsk", 3, "boeng77", new DateTime(2022, 10, 08, 05, 00, 00), "Wednesday"),
                                                 new Airlains("Sochi", 4, "boeng377", new DateTime(2023, 05, 28, 13, 00, 00), "Friday"),
                                                 new Airlains("Berlin", 5, "boeng377", new DateTime(2023, 05, 28, 13, 00, 00), "Thursday"),
-                                                new Airlains("Sochi", 6, "boeng377", new DateTime(2023, 05, 28, 13, 00, 00), "Monday"),
-                                                new Airlains("Moskow", 7, "boeng377", new DateTime(2023, 05, 28, 13, 00, 00), "Friday"),
-                                                new Airlains("Minsk", 8, "boeng377", new DateTime(2023, 05, 28, 13, 00, 00), "Thursday"),
-                                                new Airlains("Sochi", 9, "boeng377", new DateTime(2023, 05, 28, 13, 00, 00), "Monday"),
+                                                new Airlains("Sochi", 6, "boeng377", new DateTime(2023, 12, 28, 13, 00, 00), "Monday"),
+                                                new Airlains("Moskow", 7, "boeng377", new DateTime(2023, 02, 28, 13, 00, 00), "Friday"),
+                                                new Airlains("Minsk", 8, "boeng377", new DateTime(2023, 07, 28, 13, 00, 00), "Thursday"),
+                                                new Airlains("Sochi", 9, "boeng377", new DateTime(2023, 11, 28, 13, 00, 00), "Monday"),
                                                 new Airlains("Moskow", 10, "boeng377", new DateTime(2023, 05, 28, 13, 00, 00), "Friday"),
                                                 new Airlains("LA", 11, "boeng377", new DateTime(2023, 05, 28, 13, 00, 00), "Thursday"),
                                                 new Airlains("Sochi", 12, "boeng377", new DateTime(2023, 05, 28, 13, 00, 00), "Thursday"),
                                                 new Airlains("Praga", 13, "boeng37", new DateTime(2024, 03, 18, 14, 00, 00), "Friday")};
 
-            Airlains.PrintList(reys);
+            Airlains.PrintList(ref reys);
             Airlains.GoalDestination(reys);
-            Airlains.SelectionOfDay(reys);
+            Airlains.SelectionOfDay(reys, out massage);
         }
     }
 }
