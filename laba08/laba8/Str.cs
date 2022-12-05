0271e10fd5afb36e792baee9e4e2d3ab3f7e3ca6
KlrDR <ka.drach04@gmail.com>
2022-12-05 14:39:34 +0300

    контрольная

---
+++ laba08/laba8/Str.cs
@@ -0,0 +1,50 @@
+﻿namespace lab08
+{
+    public class Str
+    {
+        public static string RemoveS(string str)
+        {
+            char[] sign = { '.', ',', '!', '?', '-', ':' };
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (sign.Contains(str[i]))
+                {
+                    str = str.Remove(i, 1);
+                }
+            }
+
+            Console.WriteLine($"Without punctuation marks:\nBefore: {str}\nAfter: {(str)}\n");
+            return str;
+        }
+
+        public static string RemoveSpase(string str)
+        {
+            return str.Replace(" ", string.Empty);
+        }
+
+        public static string Upper(string str)
+        {
+            for (var i = 0; i < str.Length; i++)
+            {
+                str = str.Replace(str[i], char.ToUpper(str[i]));
+            }
+            
+            return str;
+        }
+
+        public static string Letter(string str)
+        {
+            for (var i = 0; i < str.Length; i++)
+            {
+                str = str.Replace(str[i], char.ToLower(str[i]));
+            }
+            
+            return str;
+        }
+
+        public static string AddToString(string str)
+        {
+            return str += "!!!!!!!!!!!!!!";
+        }
+    }
+}
---
+++ контрольная/kontrol2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace kontrol2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var set = new HashSet<int>() { 1, 5, 6, 7, 2 };
+            Car ooo = new Car("goovach");
+            Car nnn = new Car("Leshiiiq");
+            HashSet<Car> set2 = new HashSet<Car> {ooo,nnn};
+
+            int size = set2.Count(n => n.name == "goovach" );
+
+            Console.WriteLine(size);
+        }
+
+    }
+
+}
